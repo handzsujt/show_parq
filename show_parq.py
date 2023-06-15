322fa05d98c1d168e3875086de8dc68f16efe8a8
Handzsuj, Thomas <thomas.handzsuj@draeger.com>
2023-06-15 13:46:53 +0200

    Enable calling without argument; refactoring

--- show_parq.py
+++ show_parq.py
@@ -1,6 +1,7 @@
 import sys
 import tkinter as tk
 from tkinter.messagebox import showwarning
+from tkinter.filedialog import askopenfilename
 from pathlib import Path
 
 import numpy as np
@@ -28,29 +29,32 @@ class TestApp(tk.Frame):
 def main():
     if len(sys.argv) > 1:
         p = Path(sys.argv[1])
-        if p.suffix in [".parq", ".parquet"]:
-            df = pd.read_parquet(p)
-        elif p.suffix in [".nc", ".ncdf"]:
-            dataset = Dataset(p, "r", keepweakref=True)
-            d = {}
-            for name, var in list(dataset.variables.items()):
-                data = var[:]
-                if np.ma.is_masked(data) and "float" in str(
-                    data.dtype
-                ):  # NaNs are masked in ncdf
-                    d[name] = data.filled(np.NaN)
-                else:
-                    d[name] = data
-            df = pd.DataFrame(d)
-        else:
-            df = None
+    else:
+        p = Path(askopenfilename())
 
-        if df is None:
-            showwarning("Show Table", "File not found or file type not supported")
-        else:
-            app = TestApp(df)
-            # launch the app
-            app.mainloop()
+    if p.suffix in [".parq", ".parquet"]:
+        df = pd.read_parquet(p)
+    elif p.suffix in [".nc", ".ncdf"]:
+        dataset = Dataset(p, "r", keepweakref=True)
+        d = {}
+        for name, var in list(dataset.variables.items()):
+            data = var[:]
+            if np.ma.is_masked(data) and "float" in str(
+                data.dtype
+            ):  # NaNs are masked in ncdf
+                d[name] = data.filled(np.NaN)
+            else:
+                d[name] = data
+        df = pd.DataFrame(d)
+    else:
+        df = None
+
+    if df is None:
+        showwarning("Show Table", "File not found or file type not supported")
+    else:
+        app = TestApp(df)
+        # launch the app
+        app.mainloop()
 
 
 if __name__ == "__main__":
